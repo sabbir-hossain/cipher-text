de77390ace5b653ae6aad4704964eb0e667817cc
sabbir <sabbir.hossain.dev@gmail.com>
2021-08-25 01:08:05 +0600

    fix mouse event

--- init.js
+++ init.js
@@ -37,26 +37,51 @@ const input = {
 generateBackground(ctx, input, canvasWidth, backgroundFontSize);
 
 //Variables
-var canvasx = canvas.offsetLeft;
-var canvasy = canvas.offsetTop;
-var last_mousex = 0, last_mousey = 0;
-var mousex = 0, mousey = 0;
-var mousedown = false;
+var isDrawing, points = [ ];
+var lineWidth = 30;
 
 //Mousedown
 canvas.addEventListener('mousedown', function(e) {
-  last_mousex = parseInt(e.clientX-canvasx);
-  last_mousey = parseInt(e.clientY-canvasy);
-  mousedown = true;
+  isDrawing = true;
+  points.push({ 
+    x: e.offsetX !== e.clientX ? e.offsetX : e.clientX, 
+    y: e.clientY,
+    width: lineWidth
+  });
 });
 
 //Mouseup
 canvas.addEventListener('mouseup', function(e) {
-  mousedown = false;
+  // mousedown = false;
+  isDrawing = false;
+  points.length = 0;
 });
 
 //Mousemove
-canvas.addEventListener('mousemove', drawLine);
+canvas.addEventListener('mousemove', function(e) {
+  if (!isDrawing) return;
+
+  ctx.globalCompositeOperation = "destination-over";
+  // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  
+  points.push({ 
+    x: e.offsetX !== e.clientX ? e.offsetX : e.clientX, 
+    y: e.clientY,
+    width: lineWidth
+  });
+
+  ctx.strokeStyle = '#e09200';
+  ctx.lineJoin = ctx.lineCap = 'round';
+
+  for (var i = 1; i < points.length; i++) {
+    ctx.beginPath();
+    ctx.moveTo(points[i-1].x, points[i-1].y);
+    ctx.lineWidth = points[i].width;
+    ctx.lineTo(points[i].x, points[i].y);
+    ctx.stroke();
+  }
+})
+// canvas.addEventListener('mousemove', drawLine);
 
 function drawLine(e) {
   mousex = parseInt(e.clientX-canvasx);
@@ -80,7 +105,7 @@ function drawLine(e) {
 }
 
 const hintTextList = [
-  {"text": "FRANKFURT", "info": "One of the largest city of Germany"},
+  {"text": "FRANKFURT", "info": "One of the largest city in Germany and home to the European Central Bank"},
   {"text": "FAIZABAD", "info": "city in Pakistan/India"},
   {"text": "HALIFAX", "info": "city in Canada"},
   {"text": "JAKARTA", "info": "Capital Of Indonesia"},
